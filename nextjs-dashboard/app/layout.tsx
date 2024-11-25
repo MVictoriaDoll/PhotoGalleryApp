2444373230db010a1c3da74c7370c0d9899ad897
María Victoria Doll <mvictoriadoll@gmail.com>
2024-11-25 13:19:10 -0300

    add template

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -1,3 +1,5 @@
+import './ui/global.css';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +7,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body> {children}
+        <footer>
+          Hecho con ❤️ por Vicu
+          </footer>
+      </body>
     </html>
   );
 }
